35d56dc8c852a329965ef1150fc2d1fd38b5c2ea
Dmitry Silchenko <dmitry@desofto.com>
2018-01-18 16:26:50 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'test@gmail.com', password: 'QWEqwe123', role: 'admin')
-User.create(email: 'test2@gmail.com', password: 'QWEqwe123', role: 'operator')
+Reservation.destroy_all
+Schedule.destroy_all
+User.destroy_all
+
+User.create!(email: 'test@gmail.com', password: 'QWEqwe123', role: 'admin')
+User.create!(email: 'test2@gmail.com', password: 'QWEqwe123', role: 'operator')
+
+(0..60).each do |day|
+  Schedule.create!(date: Time.zone.today + day.days, count: (10..30).to_a.sample)
+end
